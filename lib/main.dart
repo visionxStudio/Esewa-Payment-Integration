abe8fef7974597658ffafaa0a5402a4e17dd933a
Manish Kumar Rajak <52307777+visionxStudio@users.noreply.github.com>
2020-12-19 09:44:14 +0545

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: ,
+      debugShowCheckedModeBanner: falses,
       title: 'Esewa Integration by visionx',
       theme: ThemeData(
         primarySwatch: Colors.blue,
